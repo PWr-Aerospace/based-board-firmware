1074876f208aca17b6cd8da3ae75cac046ba8116
Jan Bronicki <janbronicki@gmail.com>
2022-08-22 23:16:30 +0200

    fuck clangtidy

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 int main()
 {
     bsp_init_cpu();
-
+    int a= 10;
     while(true)
     {
         HAL_GPIO_TogglePin(PIN_LED_GPIO_Port, PIN_LED_Pin);
-        HAL_Delay(250);
+        HAL_Delay((int)250);
     }
 }
